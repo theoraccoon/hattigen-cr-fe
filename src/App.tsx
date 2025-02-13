6ecf33fe6aa9aa70eceed1f1e01b39871af33628
Conduah <nathaniel.conduah@dicebridgegmbh.onmicrosoft.com>
2025-02-13 10:19:42 +0000

    Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center h-screen font-bold text-2xl text-center'>
+      Welcome to Hattingen CR App for Code Raccoon!
+    </div>
+  )
+}
+
+export default App
